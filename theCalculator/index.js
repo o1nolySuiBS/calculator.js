c311f5651a6e7644d82a19878406b77e65340927
ihor1 <ihor1006@gmail.com>
2024-06-13 19:07:17 +0300

    Done

---
+++ theCalculator/index.js
@@ -0,0 +1,18 @@
+const displey = document.getElementById("display")
+
+function appendToDisplay(input){
+    displey.value += input
+}
+
+function clearDisplay(){
+    displey.value =""
+}
+
+function calculate(){
+    try{
+    displey.value = eval(displey.value)
+    }
+    catch (error){
+        displey.value = "Error"
+    }
+}
